dfb79101594b71be9e3a32289848fcbdbb44057c
IngJosePaez <145848487+IngJosePaez@users.noreply.github.com>
2023-10-26 17:11:09 -0300

    Add files via upload
    Curso de Programacion con el Profesor Fernando Herrera, Full Stack Developer

---
+++ app.ts
@@ -0,0 +1,41 @@
+
+
+
+function heroesThatStartsWith( nameList: string [], letter: string ) {
+
+    letter = letter.toUpperCase();
+    let newNamelist: string[] = [];
+
+
+  //  for ( let i = 0; i < nameList.length; i ++) {
+ //      let name = nameList[i];
+
+ //       if ( name.startsWith(letter)) {
+  //          newNamelist.push( name );
+
+    for ( let name of nameList ){ //Recorrer un array modo DIOS 
+
+        if ( name.startsWith(letter)) {
+            newNamelist.push( name );
+        }
+    }
+
+
+    return newNamelist;
+
+    }
+
+
+
+ 
+
+
+
+
+
+
+
+
+let heroes = ['Doom', 'Dr. Strange', 'Hulk', 'She Hulk', 'Spiderman', 'Captain Marvel'];
+let herosWithLetterS = heroesThatStartsWith( heroes, 'D');
+console.log( herosWithLetterS ); // She Hulk, Spiderman
